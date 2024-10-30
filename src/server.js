e2d01bd6942c0249427304273360642dd0dd2b86
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2024-10-31 01:04:33 +0200

    add logger to middlewares

--- src/server.js
+++ src/server.js
@@ -1,22 +1,17 @@
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
 import { env } from './utils/env.js';
 
 import contactRouter from './routers/contacts.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+// import { logger } from './middlewares/logger.js';
 
 export const setupServer = () => {
   const app = express();
   app.use(cors());
 
-  const logger = pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  });
-  //   app.use(logger);
+  // app.use(logger);
 
   app.use('/contacts', contactRouter);
 
